eae8202d11a21798396c2ba5ddf356e586024428
monis.q <monis.q@media.net>
2018-06-18 11:58:30 +0530

    small change

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const app = express();
+const read = require('node-readability');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+// exports.helloWorld = functions.https.onRequest(function(request, response) {
+//     return response.send("Hello from Firebase!");
 // });
+
+app.get("*", function (request, response) {
+    response.send("Hello from Express on Firebase!");
+});
+
+app.post("/api/getcontent", function (request, response) {
+    /** @namespace request.body.rawHtml */
+    let rawHtml = request.body && request.body.rawHtml;
+    let url = request.body && request.body.url;
+
+    read(rawHtml, function (err, article) {
+        let object = {
+            content: article.content,
+            title: article.title
+        };
+
+        response.send(JSON.stringify(object));
+        console.log("URL:", url, " Title:", article.title, ", response sent!!");
+    });
+});
+
+app.post("/api/hello", function (request, response) {
+    let name = request.body.name;
+    response.send("Hello from Express on Firebase!" + name);
+});
+
+
+const exposedFunctions = functions.https.onRequest(app);
+module.exports = {
+    exposedFunctions
+};
