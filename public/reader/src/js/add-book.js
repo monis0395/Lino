788fd588c45fad3193b3f446f84911e11b8a58c0
monis.q <monis.q@media.net>
2019-10-03 20:40:32 +0530

    renamed all to book

--- public/reader/src/js/add-book.js
+++ public/reader/src/js/add-book.js
@@ -1,16 +1,16 @@
 import { loadAnyModal } from "./modal.js";
 
 function loadAddBookModal() {
-    const modal = document.getElementById("add-link-modal");
+    const modal = document.getElementById("add-book-modal");
     const addBookBtn = document.getElementById("add-book-modal-btn");
     loadAnyModal(modal, addBookBtn)
 }
 
 function handleSubmitBtn() {
-    const submit = document.getElementById("add-link-modal-submit");
+    const submit = document.getElementById("add-book-modal-submit");
     submit.onclick = (event) => {
         event.preventDefault();
-        const linkElement = document.getElementById("add-link-modal-input");
+        const linkElement = document.getElementById("add-book-modal-input");
         const link = linkElement.value;
         console.log("link submitted", link);
     }
