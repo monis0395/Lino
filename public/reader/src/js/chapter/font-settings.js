8f40813b9ae33ee50eeaadf5cd8c287cd936e5cc
monis.q <monis.q@media.net>
2019-10-19 16:08:07 +0530

    added font settings drop up

--- public/reader/src/js/chapter/font-settings.js
+++ public/reader/src/js/chapter/font-settings.js
@@ -4,5 +4,5 @@ const modal = document.getElementById("font-settings-modal");
 const fontSettingsBtn = document.getElementById("font-settings-btn");
 
 export function fontSettingsInit() {
-    loadAnyModal(modal, fontSettingsBtn);
+    // loadAnyModal(modal, fontSettingsBtn);
 }
