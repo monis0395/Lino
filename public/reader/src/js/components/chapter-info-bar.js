75eb9e24cbab04c34d7bb8a8362a5004e36fe796
monis.q <monis.q@media.net>
2019-11-23 21:48:35 +0530

    small change

--- public/reader/src/js/components/chapter-info-bar.js
+++ public/reader/src/js/components/chapter-info-bar.js
@@ -2,6 +2,9 @@ const progressElement = document.getElementById("info-chapter-progress-bar");
 
 export function updateProgressBar(value, max) {
     const percentage = Math.floor(value / max * 100);
+    if (percentage > 100 || percentage < 0) {
+        console.log("percentage", percentage)
+    }
     const pvalue = `${percentage}%`;
     progressElement.style.width = pvalue;
 }
