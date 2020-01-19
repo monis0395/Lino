e60e5e75344a9d3f5c1717c58bc6cd54aa054859
monis.q <monis.q@media.net>
2020-01-19 19:20:42 +0530

    version bump

--- public/reader/src/service-worker.js
+++ public/reader/src/service-worker.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 30;
+const CACHE_VERSION = 31;
 const CACHE_NAME = "reader-cache";
 const CURRENT_CACHE_NAME = CACHE_NAME + '-v' + CACHE_VERSION;
 const isLocalEnv = self.location.hostname === "localhost";
